1a1011a83dcb914bcd251aa30b334fb9e5ac313f
Paul Thompson <prtsoft@gmail.com>
2012-11-23 10:33:46 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paulsapp::Application.config.secret_token = '0e0cdd7bcad5d03a202c44435ed89a73ee7b5323fc7cafa52101e8f45fa248c78fd0e3cc902d4d80f2bd74b0068e1e98d79f7aaa77cdc69e1f62f2bba6219f68'
